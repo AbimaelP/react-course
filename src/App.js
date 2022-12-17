44a56404bf095805ecb547cb68fa39da60fcd9fb
Abimael <a.bimael2000@hotmail.com>
2022-12-17 10:16:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+
+class App extends Component {
+
+  render(){
+    return (
+    <div className='App'>
+      <header className="App-header">
+      <img src={logo} className="App-logo"/>
+      </header>
+    </div>
+    )
+  }
+}
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +34,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
